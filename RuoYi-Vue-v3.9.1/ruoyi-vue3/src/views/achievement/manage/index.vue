b349d699d62cce04f60a73fc3df7714cc66fc09a
shadow <shadowaaac@gmail.com>
2026-02-04 15:57:58 +0800

    合并后能运行

--- RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/manage/index.vue
+++ RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/manage/index.vue
@@ -1,22 +1,68 @@
 <template>
   <div class="app-container">
+    <!-- 搜索表单 -->
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
       <el-form-item label="成果ID" prop="achievementId">
-        <el-input v-model="queryParams.achievementId" placeholder="请输入成果ID" clearable @keyup.enter="handleQuery"/>
+        <el-input
+            v-model="queryParams.achievementId"
+            placeholder="请输入成果ID"
+            clearable
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="比赛" prop="track">
+        <el-input
+            v-model="queryParams.track"
+            placeholder="请输入比赛"
+            clearable
+            @keyup.enter="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="届次" prop="sessionId">
-        <el-input v-model="queryParams.sessionId" placeholder="请输入届次" clearable @keyup.enter="handleQuery"/>
+        <el-input
+            v-model="queryParams.sessionId"
+            placeholder="请输入届次"
+            clearable
+            @keyup.enter="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="类别" prop="category">
         <el-select v-model="queryParams.category" placeholder="请选择类别" clearable>
-          <el-option v-for="dict in achievement_category" :key="dict.value" :label="dict.label" :value="dict.value"/>
+          <el-option
+              v-for="dict in achievement_category"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="作品名称" prop="name">
-        <el-input v-model="queryParams.name" placeholder="请输入作品名称" clearable @keyup.enter="handleQuery"/>
+        <el-input
+            v-model="queryParams.name"
+            placeholder="请输入作品名称"
+            clearable
+            @keyup.enter="handleQuery"
+        />
       </el-form-item>
-      <el-form-item label="团队名称" prop="teamName">
-        <el-input v-model="queryParams.teamName" placeholder="请输入团队名称" clearable @keyup.enter="handleQuery"/>
+      <el-form-item label="审核状态" prop="reviewStatus">
+        <el-select v-model="queryParams.reviewStatus" placeholder="请选择审核状态" clearable>
+          <el-option
+              v-for="dict in auditStatus"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="获奖时间" prop="awardTime">
+        <el-date-picker
+            v-model="queryParams.awardTimeStart"
+            type="daterange"
+            value-format="YYYY-MM-DD"
+            range-separator="至"
+            start-placeholder="开始时间"
+            end-placeholder="结束时间"
+        />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -27,68 +73,77 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['achievement:manage:add']"
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['achievement:manage:add']"
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['achievement:manage:edit']"
+            type="success"
+            plain
+            icon="Edit"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['achievement:manage:edit']"
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['achievement:manage:remove']"
+            type="danger"
+            plain
+            icon="Delete"
+            :disabled="multiple"
+            @click="handleDelete"
+            v-hasPermi="['achievement:manage:remove']"
         >删除</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['achievement:manage:export']"
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['achievement:manage:export']"
         >导出</el-button>
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="manageList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="listData" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="成果ID" align="center" prop="achievementId" />
+      <el-table-column label="比赛" align="center" prop="track" />
       <el-table-column label="届次" align="center" prop="sessionId" />
       <el-table-column label="类别" align="center" prop="category">
         <template #default="scope">
-          <dict-tag :options="achievement_category" :value="scope.row.category"/>
+          <dict-tag :options="achievement_category" :value="scope.row.category" />
         </template>
       </el-table-column>
       <el-table-column label="作品名称" align="center" prop="name" />
-      <el-table-column label="团队名称" align="center" prop="teamName" />
-      <el-table-column label="获奖级别" align="center" prop="level">
+      <el-table-column label="级别" align="center" prop="level">
         <template #default="scope">
-          <dict-tag :options="award_level_type" :value="scope.row.level"/>
+          <dict-tag :options="award_level_type" :value="scope.row.level" />
         </template>
       </el-table-column>
       <el-table-column label="获奖等级" align="center" prop="grade">
         <template #default="scope">
-          <dict-tag :options="award_rank" :value="scope.row.grade"/>
+          <dict-tag :options="award_rank" :value="scope.row.grade" />
         </template>
       </el-table-column>
-      <el-table-column label="赛道" align="center" prop="track" />
       <el-table-column label="证书编号" align="center" prop="certificateNo" />
+      <el-table-column label="组别" align="center" prop="groupId">
+        <template #default="scope">
+          <dict-tag :options="group_type" :value="scope.row.groupId" />
+        </template>
+      </el-table-column>
+      <el-table-column label="学号" align="center" prop="studentId" />
+      <el-table-column label="审核状态" align="center" prop="reviewStatus">
+        <template #default="scope">
+          <dict-tag :options="auditStatus" :value="scope.row.reviewStatus" />
+        </template>
+      </el-table-column>
       <el-table-column label="获奖时间" align="center" prop="awardTime" width="180">
         <template #default="scope">
           <span>{{ parseTime(scope.row.awardTime, '{y}-{m}-{d}') }}</span>
@@ -96,133 +151,126 @@
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
+          <el-button link type="primary" icon="Review" @click="handleReview(scope.row)" v-hasPermi="['achievement:manage:review']">详情</el-button>
           <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['achievement:manage:edit']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['achievement:manage:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
+        v-show="total > 0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
     />
 
-    <achievement-form 
-      ref="achievementFormRef" 
-      :get-fn="getManage"
-      :add-fn="addManage"
-      :update-fn="updateManage"
-      @ok="getList" 
+    <AchievementForm
+        ref="achievementFormRef"
+        :get-fn="getManage"
+        :add-fn="addManage"
+        :update-fn="updateManage"
+        @ok="getList"
     />
-
   </div>
 </template>
 
-<script setup name="Manage">
-// 1. 【重点修改】增加 getManage, addManage, updateManage 的引入，因为新组件需要这些作为 Props
-import { listManage, getManage, delManage, addManage, updateManage } from "@/api/achievement/manage"
+<script setup>
+// 字典数据
+const { achievement_category, group_type, award_rank, award_level_type, college_audit_status, school_audit_status } = useDict(
+    'achievement_category', 'group_type', 'award_rank', 'award_level_type', 'college_audit_status', 'school_audit_status'
+);
 
-// 2. 【重点修改】引入同级目录下的 AchievementForm.vue
-import AchievementForm from './AchievementForm.vue';
+// 数据和状态管理
+const queryParams = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  achievementId: null,
+  sessionId: null,
+  category: null,
+  name: null,
+  teamName: null,
+  level: null,
+  grade: null,
+  track: null,
+  certificateNo: null,
+  groupId: null,
+  awardTime: null
+});
 
-const { proxy } = getCurrentInstance()
-const { achievement_category, group_type, award_rank, award_level_type } = proxy.useDict('achievement_category', 'group_type', 'award_rank', 'award_level_type')
+const listData = ref([]);
+const loading = ref(false);
+const total = ref(0);
+const showSearch = ref(true);
+const single = ref(true);
+const multiple = ref(true);
+const ids = ref([]);
 
-const manageList = ref([])
-const loading = ref(true)
-const showSearch = ref(true)
-const ids = ref([])
-const single = ref(true)
-const multiple = ref(true)
-const total = ref(0)
+// 动态获取审核状态
+const auditStatus = computed(() => {
+  return queryParams.reviewStatus === 'college'
+      ? college_audit_status
+      : school_audit_status;
+});
 
-// 3. 【重点修改】引用名称改为 achievementFormRef
-const achievementFormRef = ref(null);
-
-const data = reactive({
-  queryParams: {
-    pageNum: 1,
-    pageSize: 10,
-    achievementId: null,
-    sessionId: null,
-    category: null,
-    name: null,
-    teamName: null,
-    level: null,
-    grade: null,
-    track: null,
-    certificateNo: null,
-    groupId: null,
-    awardTime: null,
-  }
-})
-
-const { queryParams } = toRefs(data)
-
-/** 查询成果录入列表 */
+// 获取列表数据
 function getList() {
-  loading.value = true
-  listManage(queryParams.value).then(response => {
-    manageList.value = response.rows
-    total.value = response.total
-    loading.value = false
-  })
+  loading.value = true;
+  listManage(queryParams).then(response => {
+    listData.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
 }
 
-/** 搜索按钮操作 */
-function handleQuery() {
-  queryParams.value.pageNum = 1
-  getList()
-}
+// 搜索操作
+const handleQuery = () => {
+  queryParams.pageNum = 1;
+  getList();
+};
 
-/** 重置按钮操作 */
-function resetQuery() {
-  proxy.resetForm("queryRef")
-  handleQuery()
-}
+// 重置查询条件
+const resetQuery = () => {
+  Object.keys(queryParams).forEach(key => {
+    queryParams[key] = null;
+  });
+  getList();
+};
 
-// 多选框选中数据
 function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.achievementId)
-  single.value = selection.length != 1
-  multiple.value = !selection.length
+  ids.value = selection.map(i => i.id);
+  single.value = selection.length !== 1;
+  multiple.value = !selection.length;
 }
 
-/** 4. 【重点修改】调用新组件的 open 方法 */
+// 新增操作
 function handleAdd() {
   if (achievementFormRef.value) {
     achievementFormRef.value.open();
   }
 }
 
-/** 5. 【重点修改】调用新组件的 open 方法并传入 ID */
+// 修改操作
 function handleUpdate(row) {
-  const _achievementId = row.achievementId || ids.value
+  const _achievementId = row.achievementId || ids.value[0];
   if (achievementFormRef.value) {
-    achievementFormRef.value.open(_achievementId); 
+    achievementFormRef.value.open(_achievementId);
   }
 }
 
-/** 删除按钮操作 */
+// 删除操作
 function handleDelete(row) {
-  const _achievementIds = row.achievementId || ids.value
-  proxy.$modal.confirm('是否确认删除成果录入编号为"' + _achievementIds + '"的数据项？').then(function() {
-    return delManage(_achievementIds)
-  }).then(() => {
-    getList()
-    proxy.$modal.msgSuccess("删除成功")
-  }).catch(() => {})
+  const _achievementIds = row.achievementId || ids.value;
+  proxy.$modal
+      .confirm(`是否确认删除成果编号为 "${_achievementIds}" 的数据项？`)
+      .then(() => delManage(_achievementIds))
+      .then(() => {
+        proxy.$modal.msgSuccess("删除成功");
+        getList();
+      })
+      .catch(() => {});
 }
 
-/** 导出按钮操作 */
-function handleExport() {
-  proxy.download('achievement/manage/export', {
-    ...queryParams.value
-  }, `manage_${new Date().getTime()}.xlsx`)
-}
-
-getList()
+getList();
 </script>
