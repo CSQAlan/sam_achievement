d5950dea4f073883f2435ab2d689fc491bcc4ff4
墨安安 <user001@gitblit.local>
2026-03-26 20:42:30 +0800

    解决了学生档案和教师档案的显示问题

--- RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/student/index.vue
+++ RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/student/index.vue
@@ -106,6 +106,16 @@
       </el-col>
       <el-col :span="1.5">
         <el-button
+          type="info"
+          plain
+          icon="Upload"
+          @click="handleImport"
+          v-hasPermi="['student:student:import']"
+          >导入</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
           type="warning"
           plain
           icon="Download"
@@ -129,9 +139,9 @@
       <el-table-column label="学生id" align="center" prop="studentId" />
       <el-table-column label="学生姓名" align="center" prop="name" />
       <el-table-column label="学号" align="center" prop="no" />
-      <el-table-column label="学院" align="center" prop="school" />
-      <el-table-column label="院系" align="center" prop="department" />
-      <el-table-column label="专业" align="center" prop="major" />
+      <el-table-column label="学院" align="center" prop="schoolName" />
+      <el-table-column label="院系" align="center" prop="departmentName" />
+      <el-table-column label="专业" align="center" prop="majorName" />
       <el-table-column label="班级" align="center" prop="className" />
       <el-table-column label="年级" align="center" prop="classYear" />
       <el-table-column
@@ -203,10 +213,45 @@
         </div>
       </template>
     </el-dialog>
+
+    <!-- 学生导入对话框 -->
+    <el-dialog :title="upload.title" v-model="upload.open" width="400px" append-to-body>
+      <el-upload
+        ref="uploadRef"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <div class="el-upload__tip">
+              <el-checkbox v-model="upload.updateSupport" /> 是否更新已经存在的学生数据
+            </div>
+            <span>仅允许导入xls、xlsx格式文件。</span>
+            <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitFileForm">确 定</el-button>
+          <el-button @click="upload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup name="Student">
+import { getToken } from "@/utils/auth";
 import {
   listStudent,
   getStudent,
@@ -230,6 +275,22 @@ const total = ref(0);
 const title = ref("");
 const deptOptions = ref([]);
 const deptCascaderValue = ref([]);
+
+/*** 学生导入参数 */
+const upload = reactive({
+  // 是否显示弹出层（学生导入）
+  open: false,
+  // 弹出层标题（学生导入）
+  title: "",
+  // 是否禁用上传
+  isUploading: false,
+  // 是否更新已经存在的学生数据
+  updateSupport: 0,
+  // 设置上传的请求头部
+  headers: { Authorization: "Bearer " + getToken() },
+  // 上传的地址
+  url: import.meta.env.VITE_APP_BASE_API + "/student/student/importData"
+});
 
 const data = reactive({
   form: {},
@@ -396,6 +457,33 @@ function handleExport() {
     `student_${new Date().getTime()}.xlsx`
   );
 }
+
+/** 导入按钮操作 */
+function handleImport() {
+  upload.title = "学生导入";
+  upload.open = true;
+};
+/** 下载模板操作 */
+function importTemplate() {
+  proxy.download("student/student/importTemplate", {
+  }, `student_template_${new Date().getTime()}.xlsx`)
+};
+/**文件上传中处理 */
+const handleFileUploadProgress = (event, file, fileList) => {
+  upload.isUploading = true;
+};
+/** 文件上传成功处理 */
+const handleFileSuccess = (response, file, fileList) => {
+  upload.open = false;
+  upload.isUploading = false;
+  proxy.$refs["uploadRef"].handleRemove(file);
+  proxy.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
+  getList();
+};
+/** 提交上传文件 */
+function submitFileForm() {
+  proxy.$refs["uploadRef"].submit();
+};
 
 getList();
 </script>
