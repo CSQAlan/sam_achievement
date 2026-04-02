1cb87ecd22318daa83f6ba513bff268f32683bfb
墨安安 <user001@gitblit.local>
2026-04-02 14:52:03 +0800

    完成了参与成果操作修改的移除

--- RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/manage/participated.vue
+++ RuoYi-Vue-v3.9.1/ruoyi-vue3/src/views/achievement/manage/participated.vue
@@ -8,19 +8,7 @@
         :show-add="false"
         :show-edit="false"
         :show-delete="false"
-    >
-      <!-- participated 模式可能需要特殊处理，因为 show-edit="false" -->
-      <template #action-button="{ row, handleRowUpdate, openingReviewPage, openingReviewPageId, checkEditable, permEdit }">
-        <el-button
-            link
-            type="primary"
-            icon="Edit"
-            @click="handleRowUpdate(row)"
-            :disabled="openingReviewPage"
-            :loading="openingReviewPage && String(openingReviewPageId) === String(row?.achievementId)"
-        >修改</el-button>
-      </template>
-    </AchievementManageIndex>
+    />
   </div>
 </template>
 
