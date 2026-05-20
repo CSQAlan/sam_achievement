338ad6202fedfea1a43e0d7e44596996e7bc02b3
CSQAlan <3552035393@qq.com>
2026-05-20 11:22:11 +0800

    修复bug

--- RuoYi-Vue-v3.9.1/ruoyi-admin/src/main/java/com/ruoyi/competition/controller/SessionController.java
+++ RuoYi-Vue-v3.9.1/ruoyi-admin/src/main/java/com/ruoyi/competition/controller/SessionController.java
@@ -2,6 +2,7 @@ package com.ruoyi.competition.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
